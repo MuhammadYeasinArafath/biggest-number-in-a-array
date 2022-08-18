3f8e89cedf5945bc87f44f176f00328bd64589d8
MuhammadYeasinArafath <mdyeasinarafathofficial@gmail.com>
2022-08-19 02:58:15 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# biggest-number-in-a-array
+<br>
+Homework Series
+<br>
+biggest number in a array.
+<br>
+link:
---
+++ index.js
@@ -0,0 +1,13 @@
+let array = ['105','999','087','456','111','986','666','764','453','20','975','32','98','097','014','124','888','867','909']
+
+    let max = 0;
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] > max) {
+        max = array[i];
+      }
+    }
+    alert(max);
+
+
+    //there is a problem with the code. it is not counting numbers bigger than 1000.this is the only problem i faced coding this file.
+  
